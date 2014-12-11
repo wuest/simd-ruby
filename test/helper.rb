cca86224c46ced4927e7057d58f52b369052a65e
Tina Wuest <tina.wuest@rackspace.com>
2014-12-11 07:16:36 -0600

    Move common tests to BaseTest class

--- test/helper.rb
+++ test/helper.rb
@@ -2,3 +2,4 @@ require 'test/unit'
 require 'simd'
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
+require 'basetests'
--- test/test_floatarray.rb
+++ test/test_floatarray.rb
@@ -1,74 +1,16 @@
 require 'helper'
 
-class TestFloatArray < Test::Unit::TestCase
+class TestFloatArray < BaseTests
   def setup
-    @start    = [10.0] * 8
-    @mult     = (1..8).to_a
+    super
     @start_v  = SIMD::FloatArray.new(@start)
     @mult_v   = SIMD::FloatArray.new(@mult)
   end
 
-  def test_multiplication
-    expected = @start.zip(@mult).each_with_object(:*).map(&:reduce)
-    result   = @start_v * @mult_v
-
-    assert_equal(expected, result.to_a)
-  end
-
-  def test_division
-    expected = @start.zip(@mult).each_with_object(:/).map(&:reduce)
-    result   = @start_v / @mult_v
-
-    assert_equal(expected, result.to_a)
-  end
-
-  def test_addition
-    expected = @start.zip(@mult).each_with_object(:+).map(&:reduce)
-    result   = @start_v + @mult_v
-
-    assert_equal(expected, result.to_a)
-  end
-
-  def test_subtraction
-    expected = @start.zip(@mult).each_with_object(:-).map(&:reduce)
-    result   = @start_v - @mult_v
-
-    assert_equal(expected, result.to_a)
-  end
-
-  def test_unaligned_vectors_wont_div_by_zero
-    unaligned1 = [3,4,5,6,7]
-    unaligned2 = [1,2,3,4,5]
-    unaligned3 = [2.0] * 5
-    expected   = [1.0] * 5
-
-    vector1 = SIMD::FloatArray.new(unaligned1)
-    vector2 = SIMD::FloatArray.new(unaligned2)
-    vector3 = SIMD::FloatArray.new(unaligned3)
-
-    intermediary = vector1 - vector2
-    assert_equal(unaligned3, intermediary.to_a)
-
-    result = intermediary / vector3
-    assert_equal(expected, result.to_a)
-  end
-
-  def test_creation_and_extraction
-    expected = (1..8).to_a
-    vector = SIMD::FloatArray.new(expected)
-
-    assert_equal(8, vector.length)
-    assert_equal(expected, vector.to_a)
-  end
+  build_tests(SIMD::FloatArray)
 
   def test_cannot_create_arrays_shorter_than_two
     assert_raises(ArgumentError) { SIMD::FloatArray.new([]) }
     assert_raises(ArgumentError) { SIMD::FloatArray.new([1]) }
-  end
-
-  def test_cannot_perform_operations_between_even_and_odd_vectors
-    veven = SIMD::FloatArray.new([1,2,3,4])
-    vodd  = SIMD::FloatArray.new([1,2,3])
-    assert_raises(ArgumentError) { veven * vodd }
   end
 end
