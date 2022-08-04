fde7f0a5ce1e4c3cf0d4fd32145235e87354cd09
Nathan Clark <nathan@brandnewbox.com>
2022-08-04 07:02:47 -0500

    Round Out

--- ext/simd/simd_types.h
+++ ext/simd/simd_types.h
@@ -38,16 +38,6 @@ typedef union i4v_t
 } i4v_t;
 
 /*
- * Types for BooleanArray
- */
-typedef bool bv;
-typedef union bv_t
-{
-	bv v;
-	bool;
-} bv_t;
-
-/*
  * Types for LongArray
  */
 typedef long long int __attribute__ ((vector_size (16))) l2v;
